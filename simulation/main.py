55062d873b5c68cede368a7b0478b485863c51c2
Vladyslav Kondrat <vladyslav.kondrat09@outlook.com>
2026-05-02 02:38:39 +0200

    Added the main architecture, started to code main.py

--- simulation/main.py
+++ simulation/main.py
@@ -1,6 +1,35 @@
+import numpy as np
+from mlagents_envs.environment import UnityEnvironment
+from mlagents_envs.base_env import ActionTuple
+
+from agent.rl_agent import
+from utils.logger import
+
+
 def main():
-    print("Hello from simulation!")
+    print("Starting...")
+
+    env = UnityEnvironment(file_name=None)
+    env.reset()
+
+    behavior_name = list(env.behavior_specs.keys())[0]
+    #bridge - name of the "brain" to send and receive data
+
+    num_beams = 7    #may be changed
+    agent = the_agent(input_size=num_beams)
+    logger = TensorBoardLogger()
 
 
-if __name__ == "__main__":
-    main()
+
+    #THE loop
+    episodes = 10000
+    for episode in range(episodes):
+        env.reset()
+        episode_reward=0
+        done=False
+
+
+
+
+
+
